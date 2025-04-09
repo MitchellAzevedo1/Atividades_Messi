cfefba66b5423bf1456eca41fdf9d5d17ace6096
MitchellAzevedo1 <mitchellazevedo@hotmail.com>
2025-04-09 20:28:10 -0300

    Atividade 09. Data 09.04
    Conforme solicitado

---
+++ Atividade_0904.py
@@ -0,0 +1,12 @@
+import time
+
+for num  in range(1000, 2001):
+    if num % 11 == 5:
+        print(f' O número é {num}.')
+
+        time.sleep(1)
+
+while numero <= 2000:
+    if numero % 11 == 5:
+
+        print(f'O número é {numero}.')
